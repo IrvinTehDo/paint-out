128b93e8e6725ee4fa1f0fdab619ab698f361b67
Irvin Do <irvindo@yahoo.com>
2018-02-26 05:28:58 -0500

    4 Players per room and proper placement

--- src/app.js
+++ src/app.js
@@ -7,45 +7,11 @@ const port = process.env.PORT || process.env.NODE_PORT || 55555;
 
 const rooms = {};
 
+const roomTimerFuncs = {};
+
 rooms.lobby = {
   roomName: 'lobby',
   players: {},
-};
-
-// To create and initialize a room
-const roomInit = (roomName, reqSocket) => {
-  if (rooms[roomName]) {
-    // Error Here, room is already created
-    // socket emit error to requester
-    console.log(`${roomName} already exists`);
-    reqSocket.emit('roomError', `${roomName} already exists`);
-    return false;
-  }
-
-  rooms[roomName] = {
-    roomName,
-    players: {},
-  };
-
-  console.log(`${roomName} created`);
-  return true;
-};
-
-const roomJoin = (roomName, reqSocket) => {
-  if (!rooms[roomName]) {
-    // socket emit error to requester that it doesnt exist.
-    console.log(`${roomName} does not exist`);
-    reqSocket.emit('roomError', `${roomName} does not exist`);
-    return false;
-  } else if (Object.keys(rooms[roomName].players).length >= 4) {
-    console.log(`${roomName} is full`);
-    reqSocket.emit('roomError', `${roomName} is full`);
-    return false;
-  }
-
-  console.log(`Joined Room: ${roomName}`);
-  reqSocket.emit('roomJoined', roomName);
-  return true;
 };
 
 const handler = (req, res) => {
@@ -65,6 +31,49 @@ app.listen(port);
 
 const io = socketio(app);
 
+// To create and initialize a room
+const roomInit = (roomName, reqSocket) => {
+  if (rooms[roomName]) {
+    // Error Here, room is already created
+    // socket emit error to requester
+    console.log(`${roomName} already exists`);
+    reqSocket.emit('roomError', `${roomName} already exists`);
+    return false;
+  }
+
+  rooms[roomName] = {
+    roomName,
+    status: 'waiting',
+    time: 45,
+    players: {},
+  };
+
+  roomTimerFuncs[roomName] = setInterval(() => {
+    rooms[roomName].time -= 1;
+    io.in(roomName).emit('updateTime', rooms[roomName].time);
+  }, 1000);
+
+  console.log(`${roomName} created`);
+  return true;
+};
+
+const roomJoin = (roomName, reqSocket) => {
+  if (!rooms[roomName]) {
+    // socket emit error to requester that it doesnt exist.
+    console.log(`${roomName} does not exist`);
+    reqSocket.emit('roomError', `${roomName} does not exist`);
+    return false;
+  } else if (Object.keys(rooms[roomName].players).length >= 4) {
+    console.log(`${roomName} is full`);
+    reqSocket.emit('roomError', `${roomName} is full`);
+    return false;
+  }
+
+  console.log(`Joined Room: ${roomName}`);
+  return true;
+};
+
+
 io.on('connection', (sock) => {
   const socket = sock;
   socket.join('lobby');
@@ -73,11 +82,21 @@ io.on('connection', (sock) => {
     hash: xxh.h32(`${socket.id}${new Date().getTime()}`, 0xCAFEBABE).toString(16),
     lastUpdate: new Date().getTime(),
     roomName: 'lobby',
+    x: 0,
+    y: 0,
+    prevX: 0,
+    prevY: 0,
+    destX: 0,
+    destY: 0,
+    alpha: 0,
+    height: 100,
+    width: 100,
+    color: 'red',
   };
 
   rooms.lobby.players[socket.player.hash] = socket.player;
 
-  socket.emit('joined', socket.player);
+  socket.emit('joined', socket.player, rooms.lobby);
 
   socket.on('createRoom', (roomName) => {
     if (roomInit(roomName, socket)) {
@@ -87,9 +106,46 @@ io.on('connection', (sock) => {
         } else {
           delete rooms[socket.roomName].players[socket.player.hash];
         }
-        socket.join(roomName);
+        // socket.join(roomName);
         socket.player.roomName = roomName;
         rooms[roomName].players[socket.player.hash] = socket.player;
+
+        const playerKeys = Object.keys(rooms[roomName].players);
+        for (let i = 0; i < playerKeys.length; i++) {
+          if (socket.player.hash === rooms[roomName].players[playerKeys[i]].hash) {
+            if (i === 0) {
+              socket.player.x = 0;
+              socket.player.y = 0;
+              socket.player.color = 'red';
+              rooms[roomName].players[socket.player.hash].x = socket.player.x;
+              rooms[roomName].players[socket.player.hash].y = socket.player.y;
+              rooms[roomName].players[socket.player.hash].color = socket.player.color;
+            } else if (i === 1) {
+              socket.player.x = 700;
+              socket.player.y = 0;
+              socket.player.color = 'blue';
+              rooms[roomName].players[socket.player.hash].x = socket.player.x;
+              rooms[roomName].players[socket.player.hash].y = socket.player.y;
+              rooms[roomName].players[socket.player.hash].color = socket.player.color;
+            } else if (i === 2) {
+              socket.player.x = 0;
+              socket.player.y = 500;
+              socket.player.color = 'green';
+              rooms[roomName].players[socket.player.hash].x = socket.player.x;
+              rooms[roomName].players[socket.player.hash].y = socket.player.y;
+              rooms[roomName].players[socket.player.hash].color = socket.player.color;
+            } else if (i === 3) {
+              socket.player.x = 700;
+              socket.player.y = 500;
+              socket.player.color = 'purple';
+              rooms[roomName].players[socket.player.hash].x = socket.player.x;
+              rooms[roomName].players[socket.player.hash].y = socket.player.y;
+              rooms[roomName].players[socket.player.hash].color = socket.player.color;
+            }
+          }
+        }
+        socket.join(roomName);
+        socket.emit('roomJoined', rooms[roomName]);
         console.dir(rooms);
       }
     }
@@ -102,11 +158,87 @@ io.on('connection', (sock) => {
       } else {
         delete rooms[socket.roomName].players[socket.player.hash];
       }
-      socket.join(roomName);
+
       socket.player.roomName = roomName;
       rooms[roomName].players[socket.player.hash] = socket.player;
+
+      const playerKeys = Object.keys(rooms[roomName].players);
+      for (let i = 0; i < playerKeys.length; i++) {
+        if (socket.player.hash === rooms[roomName].players[playerKeys[i]].hash) {
+          if (i === 0) {
+            socket.player.x = 0;
+            socket.player.y = 0;
+            socket.player.prevX = 0;
+            socket.player.prevY = 0;
+            socket.player.destX = 0;
+            socket.player.destY = 0;
+            socket.player.color = 'red';
+            rooms[roomName].players[socket.player.hash].x = socket.player.x;
+            rooms[roomName].players[socket.player.hash].y = socket.player.y;
+            rooms[roomName].players[socket.player.hash].prevX = socket.player.prevX;
+            rooms[roomName].players[socket.player.hash].prevY = socket.player.prevY;
+            rooms[roomName].players[socket.player.hash].destX = socket.player.destX;
+            rooms[roomName].players[socket.player.hash].destY = socket.player.destY;
+            rooms[roomName].players[socket.player.hash].color = socket.player.color;
+          } else if (i === 1) {
+            socket.player.x = 700;
+            socket.player.y = 0;
+            socket.player.prevX = 700;
+            socket.player.prevY = 0;
+            socket.player.destX = 700;
+            socket.player.destY = 0;
+            socket.player.color = 'blue';
+            rooms[roomName].players[socket.player.hash].x = socket.player.x;
+            rooms[roomName].players[socket.player.hash].y = socket.player.y;
+            rooms[roomName].players[socket.player.hash].prevX = socket.player.prevX;
+            rooms[roomName].players[socket.player.hash].prevY = socket.player.prevY;
+            rooms[roomName].players[socket.player.hash].destX = socket.player.destX;
+            rooms[roomName].players[socket.player.hash].destY = socket.player.destY;
+            rooms[roomName].players[socket.player.hash].color = socket.player.color;
+          } else if (i === 2) {
+            socket.player.x = 0;
+            socket.player.y = 500;
+            socket.player.prevX = 0;
+            socket.player.prevY = 500;
+            socket.player.destX = 0;
+            socket.player.destY = 500;
+            socket.player.color = 'green';
+            rooms[roomName].players[socket.player.hash].x = socket.player.x;
+            rooms[roomName].players[socket.player.hash].y = socket.player.y;
+            rooms[roomName].players[socket.player.hash].prevX = socket.player.prevX;
+            rooms[roomName].players[socket.player.hash].prevY = socket.player.prevY;
+            rooms[roomName].players[socket.player.hash].destX = socket.player.destX;
+            rooms[roomName].players[socket.player.hash].destY = socket.player.destY;
+            rooms[roomName].players[socket.player.hash].color = socket.player.color;
+          } else if (i === 3) {
+            socket.player.x = 700;
+            socket.player.y = 500;
+            socket.player.prevX = 700;
+            socket.player.prevY = 500;
+            socket.player.destX = 700;
+            socket.player.destY = 500;
+            socket.player.color = 'purple';
+            rooms[roomName].players[socket.player.hash].x = socket.player.x;
+            rooms[roomName].players[socket.player.hash].y = socket.player.y;
+            rooms[roomName].players[socket.player.hash].prevX = socket.player.prevX;
+            rooms[roomName].players[socket.player.hash].prevY = socket.player.prevY;
+            rooms[roomName].players[socket.player.hash].destX = socket.player.destX;
+            rooms[roomName].players[socket.player.hash].destY = socket.player.destY;
+            rooms[roomName].players[socket.player.hash].color = socket.player.color;
+            rooms[roomName].players[socket.player.hash].color = socket.player.color;
+          }
+        }
+      }
+      socket.join(roomName);
+      socket.emit('roomJoined', rooms[roomName]);
       console.dir(rooms);
     }
+  });
+
+  socket.on('movementUpdate', (data) => {
+    socket.player = data;
+    socket.player.lastUpdate = new Date().getTime();
+    socket.broadcast.to(socket.player.roomName).emit('updatedMovement', socket.player);
   });
 
   socket.on('disconnect', () => {
